c78df4cbdd274d3f56c88feb609b3d6a081c6d08
thanhnam <95249354+Thanhnampy@users.noreply.github.com>
2022-03-23 02:56:42 +0700

    Khao_sat_KH.py

--- khảo_sát_khách_hàng.py
+++ khảo_sát_khách_hàng.py
@@ -1,19 +1,19 @@
 def KhaosatKH():
-    tongBill = float(input("Nhập bill: "))
-    if tongBill >= 150:
-        price = tongBill - 50
+    bill = float(input("Nhập bill: "))
+    if bill >= 150:
+        price = bill - 50
         print("Discount 50 USD")
         print(f"Phải trả {price}")
-    elif tongBill >= 100:
-        price = tongBill - 25
+    elif bill >= 100:
+        price = bill - 25
         print("Discount 25 USD")
         print(f"Phải trả {price}")
-    elif tongBill >= 75:
-        price = tongBill - 15
+    elif bill >= 75:
+        price = bill - 15
         print("Discount 15 USD ")
         print(f"Phải trả {price}")
-    elif tongBill < 75: 
-        price = tongBill
+    else: 
+        price = bill
         print("No discount") 
         print(f"Phải trả {price}")   
 if __name__ == "__main__":
